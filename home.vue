fd707fe29cc1426bda57b3e72691f9ed648ff79a
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-10-16 14:46:35 -0400

    Pacific Pearl 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -25,11 +25,11 @@
                                 </div>
                             </div>
                             <div v-else-if="!banner.url">
-                                <div class="banner_image" v-bind:style="{ backgroundImage: 'url(' + banner.image_url + ')' }"></div>
+                                <div class="banner_image" v-bind:style="{ backgroundImage: 'url('//codecloud.cdn.speedyrails.net/sites/5da761e56e6f643378020000/image/jpeg/1571250863000/pp_home_1920x470.jpg')' }"></div>
                             </div>
                             <div v-else>
                                 <a :href="banner.url">
-                                    <div class="banner_image" v-bind:style="{ backgroundImage: 'url(' + banner.image_url + ')' }"></div>
+                                    <div class="banner_image" v-bind:style="{ backgroundImage: 'url('//codecloud.cdn.speedyrails.net/sites/5da761e56e6f643378020000/image/jpeg/1571250863000/pp_home_1920x470.jpg')' }"></div>
                                 </a>
                             </div>
                         </div>
