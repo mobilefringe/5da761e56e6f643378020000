1736eaaf3e9852015201b8ea23feb6c02e81ddb9
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-10-16 16:32:48 -0400

    Pacific Pearl 1.0  /specialty_leasing.vue

--- specialty_leasing.vue
+++ specialty_leasing.vue
@@ -54,7 +54,7 @@
             },
             created() {
                 this.loadData().then(response => {
-                    var temp_repo = this.findRepoByName('Leasing Banner');
+                    var temp_repo = this.findRepoByName('Specialty Leasing Banner');
                     if(temp_repo != null && temp_repo !==undefined) {
                         this.pageBanner = temp_repo.images[0];
                     } else {
