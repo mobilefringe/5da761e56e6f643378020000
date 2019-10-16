60d1399280a7b12eafdc5b1a92c67e1ab2c3e6e6
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-10-16 15:38:42 -0400

    Pacific Pearl 1.0  /header.vue

--- header.vue
+++ header.vue
@@ -12,11 +12,21 @@
                                 </a>
                             </span>
                         </div>
+                        
+                        <!--dynamic -->
+                        <!--<div class="site_logo center-block" :class="{ mini_logo: scrollY }">-->
+                        <!--    <a href="/">-->
+                        <!--        <img :alt="siteInfo.siteName" :src="siteInfo.siteLogo">-->
+                        <!--    </a>-->
+                        <!--</div>-->
+                        
+                        <!--hardcoded-->
                         <div class="site_logo center-block" :class="{ mini_logo: scrollY }">
                             <a href="/">
-                                <img :alt="siteInfo.siteName" :src="siteInfo.siteLogo">
+                                <img :alt="siteInfo.siteName" src="//codecloud.cdn.speedyrails.net/sites/5da761e56e6f643378020000/image/png/1571251694000/pp_600x180_logo.png">
                             </a>
                         </div>
+                        
                         <div @click="showMenu = !showMenu" :class="{ open: showMenu }" id="menu-icon">
                             <span></span>
                             <span></span>
