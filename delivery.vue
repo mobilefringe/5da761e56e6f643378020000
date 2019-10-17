a530544b989063504f6de7e8b9f884b28772fada
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-10-17 15:31:46 -0400

    Pacific Pearl 1.0  /delivery.vue

--- delivery.vue
+++ delivery.vue
@@ -124,7 +124,7 @@
             },
             created (){
                 this.loadData().then(response => {
-                    var temp_repo = this.findRepoByName('Dine Banner');
+                    var temp_repo = this.findRepoByName('Dinei Banner');
                     if (temp_repo !== null && temp_repo !== undefined) {
                        temp_repo = temp_repo.images;
                        this.pageBanner = temp_repo[0];
