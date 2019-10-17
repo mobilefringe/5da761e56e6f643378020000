c7d049ea6300b860323225d85590e7e387e3482d
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-10-17 14:57:45 -0400

    Pacific Pearl 1.0  /main.js

--- main.js
+++ main.js
@@ -41,7 +41,7 @@ require.config({
         // 'vue-masonry-plugin' : "https://codecloud.cdn.speedyrails.net/sites/5b1550796e6f641cab010000/application/javascript/1529435130000/masonrymod",
         //mappplic-png--map dependencies
         'mousewheel' : 'https://cdnjs.cloudflare.com/ajax/libs/jquery-mousewheel/3.1.13/jquery.mousewheel.min',
-        'mapplic' : 'https://mmvue.codecloudapp.com/mapplic',
+        'mapplic-png-map' : 'https://mmvue.codecloudapp.com/mapplic_png.vue?noext',
         'hammer' : 'https://codecloud.cdn.speedyrails.net/sites/59bac7db6e6f644f22ba0000/text/javascript/1484859750000/hammer.min',
         'mapplic-map' : 'https://mmvue.codecloudapp.com/mapplic.vue?noext',
         'loader': 'https://mmvue.codecloudapp.com/loader.vue?noext',
