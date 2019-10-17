6378fc50595fd914e24dd26160e776a005a80269
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-10-17 13:18:46 -0400

    Pacific Pearl 1.0  /specialty_leasing.vue

--- specialty_leasing.vue
+++ specialty_leasing.vue
@@ -74,7 +74,7 @@
                     }
 
                     if(response && response[1]){
-                        this.main = response[1].data;
+                        this.main = response[0].data;
                         if(response[1].data && response[1].data.subpages && response[1].data.subpages[0]){
                             this.leasingInfo = response[1].data.subpages[0]
                         }
