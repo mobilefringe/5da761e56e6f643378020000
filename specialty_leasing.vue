67f68aea4605fc0574137b7ebb3f0b94b0ee6ce7
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-10-17 13:18:15 -0400

    Pacific Pearl 1.0  /specialty_leasing.vue

--- specialty_leasing.vue
+++ specialty_leasing.vue
@@ -93,7 +93,7 @@
                 loadData: async function () {
                     this.property.mm_host = this.property.mm_host.replace("http:", "");
                     try {
-                        let results = await Promise.all([this.$store.dispatch("getData", "repos"), this.$store.dispatch('LOAD_PAGE_DATA', {url: this.property.mm_host + "/pages/landing-pop-up-specialty-leasing.json"})]);
+                        let results = await Promise.all([this.$store.dispatch('LOAD_PAGE_DATA', { url: this.property.mm_host + "/pages/" + Site.subdomain + "-pop-up-specialty-leasing.json" })]);
                         return results;
                     } catch (e) {
                         console.log("Error loading data: " + e.message);
