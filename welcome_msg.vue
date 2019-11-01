8662eb3d85b19e81f4bc85a88055dc942edf0d06
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-11-01 13:42:23 -0400

    Pacific Pearl 1.0  /welcome_msg.vue

---
+++ welcome_msg.vue
@@ -0,0 +1,22 @@
+<template>
+    <div v-if="property" class="main_container">
+        <div class="message_content">
+            <h1 class="center">{{ property.welcome_message }}</h1>
+            <!--<p class="center">{{ property.description }}</p> -->
+            <p class="center">Pacific Pearl delivers the ultimate shopping experience in a distinctive outdoor setting. Immerse your shopping senses in a unique blend of specialty retailers and restaurants.</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    define(["Vue", "vuex"], function(Vue, Vuex){
+        return Vue.component("messages-component", {
+            template: template, // the variable template will be injected
+            computed: {
+                ...Vuex.mapGetters([
+                    'property'
+                ])
+            }
+        })
+    });
+</script>
