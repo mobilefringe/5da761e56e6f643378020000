8f41a9adad9014ba590de77b237b5a48a0f342f1
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-11-27 12:57:30 -0500

    Pacific Pearl 1.1  /welcome_msg.vue

--- welcome_msg.vue
+++ welcome_msg.vue
@@ -3,7 +3,6 @@
         <div class="message_content">
             <h1 class="center">{{ property.welcome_message }}</h1>
             <p class="center">{{ property.description }}</p> 
-            <!--<p class="center">Pacific Pearl delivers the ultimate shopping experience in a distinctive outdoor setting. Immerse your shopping senses in a unique blend of specialty retailers and restaurants.</p>-->
         </div>
     </div>
 </template>
