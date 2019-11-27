45a9a08e13e3b6c0d2e484b98042925f4069990f
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-11-27 15:15:45 -0500

    Pacific Pearl 1.1  /location.vue

--- location.vue
+++ location.vue
@@ -22,7 +22,7 @@
                 </div>
                 <div class="main_container">
                     <div class="location_map">
-                        <iframe title="Google Map for Pacific Pearl." :src="propertyAddress()" width="100%" height="490" frameborder="0" style="border:0" allowfullscreen></iframe>
+                        <iframe aria-hidden="true" title="Google Map for Pacific Pearl." :src="propertyAddress()" width="100%" height="490" frameborder="0" style="border:0" allowfullscreen></iframe>
                     </div>
                 </div>
                 <div class="main_container">
