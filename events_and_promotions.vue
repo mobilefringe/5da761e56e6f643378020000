f19aa4755daa3e06bbec86c8259ee9ed630eae56
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-11-27 13:33:31 -0500

    Pacific Pearl 1.1  /events_and_promotions.vue

--- events_and_promotions.vue
+++ events_and_promotions.vue
@@ -24,7 +24,7 @@
                         <div v-if="eventList" v-for="(events, key) in eventList">
                             <div class="row">
                                 <div class="col-md-12">
-                                    <h3 class="event_date_heading">{{ key }}</h3> 
+                                    <h2 class="event_date_heading">{{ key }}</h2> 
                                 </div>
                             </div>
                             <div class="row event_container" v-for="event in events">
