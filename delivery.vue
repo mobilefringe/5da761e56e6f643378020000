14968c6b492a430c283ef1670ebf32c3a6a792e2
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2020-05-21 14:44:41 -0400

    Pacific Pearl 1.1  /delivery.vue

--- delivery.vue
+++ delivery.vue
@@ -65,15 +65,11 @@
                                                     <div class="store_text"><h2>{{ store.name }}</h2></div>
                                                 </div>
                                             </div>
-                                			<div class="store_tag" v-if="store.total_published_promos">
-            									<div class="store_tag_text">Promotion</div>
-            								</div>
-            								<div class="store_tag" v-if="!store.total_published_promos && store.is_coming_soon_store">
-            									<div class="store_tag_text">Coming Soon</div>
-            								</div>
-            								<div class="store_tag" v-if="!store.total_published_promos && !store.is_coming_soon_store && store.is_new_store">
-            									<div class="store_tag_text">New Store</div>
-            								</div>
+                                			<div v-if="store.store_flags">
+            								    <div class="store_tag" v-for="(tag, index) in store.store_flags">
+                                                    <div class="store_tag_text">{{ tag }}</div>
+            								    </div>
+                                            </div>
             								<div class="store_details">
             								    <div class="store_text"><h2>{{ store.name }}</h2></div>    
             								</div>
